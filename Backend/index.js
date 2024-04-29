b6bc48dc56592d496deee3e499dcae8ce70f698e
gabrielechini-ittbuonarroti <gabriele.chini@buonarroti.tn.it>
2024-04-29 19:42:36 +0200

    quiz answer e login

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const axios = require('axios');
 const mysql = require('mysql');
+const session = require('express-session');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+// Configurazione del middleware di sessione
+app.use(session({
+  secret: 'segreto', // Chiave segreta per la firma delle sessioni
+  resave: false, // Non salvare la sessione se non viene modificata
+  saveUninitialized: false, // Non salvare la sessione per i nuovi utenti non autenticati
+}));
 
 // Configurazione del database
 const connection = mysql.createConnection({
@@ -149,7 +157,48 @@ app.get('/quiz', async (req, res) => {
   }
 });
 
+app.post('/quiz/answer', (req, res) => {
+  const { userAnswer, correctAnswer } = req.body;
 
+  // Verifica se la risposta dell'utente è corretta confrontandola con la risposta corretta
+  const isCorrect = userAnswer === correctAnswer;
+  if(isCorrect){
+    //Aumento il livello del pokemon
+  }
+  // Invia la risposta (corretta o errata) al frontend
+  res.json({ isCorrect });
+});
+
+app.use(bodyParser.json());
+
+
+
+// Route per la gestione delle richieste di login
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  const sql = 'SELECT * FROM users WHERE username = ? AND password = ?';
+  db.query(sql, [username, password], (err, result) => {
+    if (err) {
+      res.status(500).send('Errore del server');
+    } else if (result.length > 0) {
+      // Creazione della sessione dopo il login con successo
+      req.session.user = username;
+      res.status(200).send('Accesso effettuato con successo');
+    } else {
+      res.status(401).send('Credenziali non valide');
+    }
+  });
+});
+
+function requireAuth(req, res, next) {
+  if (req.session.user) {
+    // L'utente è autenticato, procedi alla prossima route o middleware
+    next();
+  } else {
+    // L'utente non è autenticato, reindirizzalo alla pagina di login o restituisci un errore
+    res.status(401).send('Utente non autenticato');
+  }
+}
 
 app.listen(5000, () => {
   console.log('Il server è in ascolto sulla porta 5000');
