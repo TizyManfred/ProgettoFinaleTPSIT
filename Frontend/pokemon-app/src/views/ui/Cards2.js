0b933913332c8e4e6c30eb755dd5cb2d1ffb9141
max.fedrizzi <max.fedrizzi@buonarroti.tn.it>
2024-05-13 16:53:14 +0200

    loader fatto anche su pokedex

--- Frontend/pokemon-app/src/views/ui/Cards2.js
+++ Frontend/pokemon-app/src/views/ui/Cards2.js
@@ -11,23 +11,21 @@ import {
     Col,
   } from "reactstrap";
   import Blog2 from "../../components/dashboard/Blog2";
+  import Loader from "../../layouts/loader/Loader";
   //import bg1 from "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png";
   import bg2 from "../../assets/images/bg/bg2.jpg";
   import bg3 from "../../assets/images/bg/bg3.jpg";
   import bg4 from "../../assets/images/bg/bg4.jpg";
   import axios from 'axios';
   import React, { useState, useEffect } from 'react';
-  
- 
-
   const BlogData = [
-    
   ];
   
 
   const Cards = () => {
     const [data, setData] = useState([]);
-  
+    const [loading, setLoading] = useState(true); // Aggiungi lo stato per il caricamento
+    
     useEffect(() => {
       axios.get('http://localhost:50000/api/pokedex')
         .then(response => {
@@ -35,13 +33,18 @@ import {
         })
         .catch(error => {
           console.error('Errore nella richiesta API:', error);
-        });
+        })
+        .finally(() => {
+        setLoading(false);
+    })
     }, []);
     console.log(data);
-    return (<div>
-      {/* --------------------------------------------------------------------------------*/}
-      {/* Card-1*/}
-      {/* --------------------------------------------------------------------------------*/}
+    return (
+    <div>
+      {loading ? (
+        <Loader/>
+      ) : (
+        <div>
       <h1 className="mb-3">Pokédex</h1>
       <Row>
         {data.map((blg, index) => (
@@ -59,8 +62,8 @@ import {
           </Col>
         ))}
       </Row>
+      </div>
+      )}
     </div>
-    );
-  };
+  )};
   export default Cards;
-  
